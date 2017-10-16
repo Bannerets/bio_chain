1c22c7b3a62815ba1471a8ca4a4515ad65b25ff4
EyeZiS <tabhooked@gmail.com>
2017-10-16 14:50:22 +0200

    make docstrings more helpful

--- commands.py
+++ commands.py
@@ -3,14 +3,14 @@ from util import *
 
 
 def cmd_help(db, update, directed, command_args):
-    """shows this message"""
+    """/help - shows this message"""
     if not directed:
         return
 
     update.message.reply_text(help_text, parse_mode='Markdown')
 
 def cmd_pin(db, update, directed, command_args):
-    """quotes the current pin message"""
+    """/pin - quotes the current pin message"""
     if update.message.chat.id != CHAT_ID:
         update.message.reply_text('Sorry, I can only do that in the official group')
         return
@@ -21,8 +21,11 @@ def cmd_pin(db, update, directed, command_args):
 help_text = []
 for attr in dir(commands):
     if attr.startswith('cmd_'):
-        help_text.append('/{} - {}'.format(
-            attr[4:],
-            getattr(commands, attr).__doc__ or 'no help available'
-        ))
+        doc = getattr(commands, attr).__doc__
+        if doc:
+            help_text.append(doc)
+        else:
+            help_text.append('/{} - no help available'.format(
+                attr[4:],
+            ))
 help_text = '\n'.join(help_text)
